473155f65a780906853dcea67be496a8f06edb7f
Sagar Poddar <sagarkrp@outlook.com>
2026-01-18 21:04:40 +0530

    Add placeholder on home, animation to loading image and change error image

---
+++ src/utils/placeholderData.js
@@ -0,0 +1,376 @@
+const homePagePlaceholder = {
+  kind: "youtube#searchListResponse",
+  nextPageToken: "",
+  regionCode: "",
+  pageInfo: {
+    totalResults: null,
+    resultsPerPage: null,
+  },
+  items: [
+    {
+      kind: "",
+      id: {
+        kind: "",
+        videoId: "",
+      },
+      snippet: {
+        publishedAt: "",
+        channelId: "",
+        title: "",
+        description: "",
+        thumbnails: {
+          default: {
+            url: "",
+            width: null,
+            height: null,
+          },
+          medium: {
+            url: "",
+            width: null,
+            height: null,
+          },
+          high: {
+            url: "",
+            width: null,
+            height: null,
+          },
+        },
+        channelTitle: "",
+        liveBroadcastContent: "",
+        publishTime: "",
+      },
+    },
+    {
+      kind: "",
+      id: {
+        kind: "",
+        videoId: "",
+      },
+      snippet: {
+        publishedAt: "",
+        channelId: "",
+        title: "",
+        description: "",
+        thumbnails: {
+          default: {
+            url: "",
+            width: null,
+            height: null,
+          },
+          medium: {
+            url: "",
+            width: null,
+            height: null,
+          },
+          high: {
+            url: "",
+            width: null,
+            height: null,
+          },
+        },
+        channelTitle: "",
+        liveBroadcastContent: "",
+        publishTime: "",
+      },
+    },
+    {
+      kind: "",
+      id: {
+        kind: "",
+        videoId: "",
+      },
+      snippet: {
+        publishedAt: "",
+        channelId: "",
+        title: "",
+        description: "",
+        thumbnails: {
+          default: {
+            url: "",
+            width: null,
+            height: null,
+          },
+          medium: {
+            url: "",
+            width: null,
+            height: null,
+          },
+          high: {
+            url: "",
+            width: null,
+            height: null,
+          },
+        },
+        channelTitle: "",
+        liveBroadcastContent: "",
+        publishTime: "",
+      },
+    },
+    {
+      kind: "",
+      id: {
+        kind: "",
+        videoId: "",
+      },
+      snippet: {
+        publishedAt: "",
+        channelId: "",
+        title: "",
+        description: "",
+        thumbnails: {
+          default: {
+            url: "",
+            width: null,
+            height: null,
+          },
+          medium: {
+            url: "",
+            width: null,
+            height: null,
+          },
+          high: {
+            url: "",
+            width: null,
+            height: null,
+          },
+        },
+        channelTitle: "",
+        liveBroadcastContent: "",
+        publishTime: "",
+      },
+    },
+    {
+      kind: "",
+      id: {
+        kind: "",
+        videoId: "",
+      },
+      snippet: {
+        publishedAt: "",
+        channelId: "",
+        title: "",
+        description: "",
+        thumbnails: {
+          default: {
+            url: "",
+            width: null,
+            height: null,
+          },
+          medium: {
+            url: "",
+            width: null,
+            height: null,
+          },
+          high: {
+            url: "",
+            width: null,
+            height: null,
+          },
+        },
+        channelTitle: "",
+        liveBroadcastContent: "",
+        publishTime: "",
+      },
+    },
+    {
+      kind: "",
+      id: {
+        kind: "",
+        videoId: "",
+      },
+      snippet: {
+        publishedAt: "",
+        channelId: "",
+        title: "",
+        description: "",
+        thumbnails: {
+          default: {
+            url: "",
+            width: null,
+            height: null,
+          },
+          medium: {
+            url: "",
+            width: null,
+            height: null,
+          },
+          high: {
+            url: "",
+            width: null,
+            height: null,
+          },
+        },
+        channelTitle: "",
+        liveBroadcastContent: "",
+        publishTime: "",
+      },
+    },
+    {
+      kind: "",
+      id: {
+        kind: "",
+        videoId: "",
+      },
+      snippet: {
+        publishedAt: "",
+        channelId: "",
+        title: "",
+        description: "",
+        thumbnails: {
+          default: {
+            url: "",
+            width: null,
+            height: null,
+          },
+          medium: {
+            url: "",
+            width: null,
+            height: null,
+          },
+          high: {
+            url: "",
+            width: null,
+            height: null,
+          },
+        },
+        channelTitle: "",
+        liveBroadcastContent: "",
+        publishTime: "",
+      },
+    },
+    {
+      kind: "",
+      id: {
+        kind: "",
+        videoId: "",
+      },
+      snippet: {
+        publishedAt: "",
+        channelId: "",
+        title: "",
+        description: "",
+        thumbnails: {
+          default: {
+            url: "",
+            width: null,
+            height: null,
+          },
+          medium: {
+            url: "",
+            width: null,
+            height: null,
+          },
+          high: {
+            url: "",
+            width: null,
+            height: null,
+          },
+        },
+        channelTitle: "",
+        liveBroadcastContent: "",
+        publishTime: "",
+      },
+    },
+    {
+      kind: "",
+      id: {
+        kind: "",
+        videoId: "",
+      },
+      snippet: {
+        publishedAt: "",
+        channelId: "",
+        title: "",
+        description: "",
+        thumbnails: {
+          default: {
+            url: "",
+            width: null,
+            height: null,
+          },
+          medium: {
+            url: "",
+            width: null,
+            height: null,
+          },
+          high: {
+            url: "",
+            width: null,
+            height: null,
+          },
+        },
+        channelTitle: "",
+        liveBroadcastContent: "",
+        publishTime: "",
+      },
+    },
+    {
+      kind: "",
+      id: {
+        kind: "",
+        videoId: "",
+      },
+      snippet: {
+        publishedAt: "",
+        channelId: "",
+        title: "",
+        description: "",
+        thumbnails: {
+          default: {
+            url: "",
+            width: null,
+            height: null,
+          },
+          medium: {
+            url: "",
+            width: null,
+            height: null,
+          },
+          high: {
+            url: "",
+            width: null,
+            height: null,
+          },
+        },
+        channelTitle: "",
+        liveBroadcastContent: "",
+        publishTime: "",
+      },
+    },
+    {
+      kind: "",
+      id: {
+        kind: "",
+        videoId: "",
+      },
+      snippet: {
+        publishedAt: "",
+        channelId: "",
+        title: "",
+        description: "",
+        thumbnails: {
+          default: {
+            url: "",
+            width: null,
+            height: null,
+          },
+          medium: {
+            url: "",
+            width: null,
+            height: null,
+          },
+          high: {
+            url: "",
+            width: null,
+            height: null,
+          },
+        },
+        channelTitle: "",
+        liveBroadcastContent: "",
+        publishTime: "",
+      },
+    },
+  ],
+};
+
+export { homePagePlaceholder };
